5fc4ac3c64924409facf6502c59b47fd03efc4c5
Anvar <anvarsam@mail.ru>
2020-10-31 07:32:13 +0500

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,167 @@
+"use strict";
+
+// let start = new Date();
+
+// for (let i = 0; i <= 100000; i++) {
+//     let s = i ** 3;
+// }
+
+// let end = new Date();
+
+// console.log(`Цикл отработал ${end-start} миллисекунд`);
+
+/// ********
+
+// const box = document.querySelector('.box'),
+//       btn = document.querySelector('button');  
+
+
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+
+// btn.addEventListener('click',() => {
+//     box.style.height = box.scrollHeight+'px';
+//     console.log(box.scrollTop);
+// });
+
+// console.log(box.getBoundingClientRect().top);
+// const style = window.getComputedStyle(box);
+// console.log(style.display);
+
+//** Constructor */
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`);
+//     };
+// }
+
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name} вышел из системы`);
+// };
+
+// const ivan = new User('Ivan', 28);
+// const sasha = new User('Sasha', 32);
+
+// ivan.hello();
+// sasha.hello();
+
+// ivan.exit();
+// sasha.exit();
+
+// console.log(ivan);
+// console.log(sasha);
+
+//****** Контекст вызова ***** ///////////
+
+// function showThis(a,b) {
+//     console.log(this);
+//     function sum(){
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+
+// showThis(1,6);
+
+// const obj = {
+//     a : 20,
+//     b : 15,
+//     sum : function() {
+//         console.log(this);
+//     }
+// };
+
+// obj.sum();
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name+' '+surname);
+// }
+
+// const user = {
+//     name : 'John'
+// };
+
+// sayName.call(user,'Smith');
+// sayName.apply(user,['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+
+// console.log(double(3));
+// console.log(double(14));
+
+// 1) Обычная функция : this = window, но если use strict тогда this = undefined
+// 2) Контекст у методов объектов - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this:call, apply, bind
+
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', function() {
+//     //console.log(this);
+//     this.style.backgroundColor = 'red';
+// });
+
+// const obj = {
+//     num : 5,
+//     sayNumber : function() {
+//         const say = () => {
+//             console.log(this);
+//         }
+
+//         say();
+//     }
+// };
+
+// obj.sayNumber();
+
+// const double = a => a * 2;
+// console.log(double(5));
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class ColoredRectanglewithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст ${this.text} цвет ${this.bgColor}`);
+    } 
+}
+
+// const square = new Rectangle(10,3);
+// const big = new Rectangle(10,10);
+
+// console.log(square.calcArea());
+// console.log(big.calcArea());
+
+const div = new ColoredRectanglewithText(25,10,'Text', 'red');
+
+div.showMyProps();
+console.log(div.calcArea());
+
+
+
+
+
